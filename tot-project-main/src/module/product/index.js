afe0c141766d859abd161775ade5f7684392346b
Wijikhur <w.khurniawati@gmail.com>
2023-08-16 11:05:21 +0700

    first commit

--- tot-project-main/src/module/product/index.js
+++ tot-project-main/src/module/product/index.js
@@ -1,12 +1,28 @@
 import Layout from "@/components/Layout";
+import { Button, Title } from "@mantine/core";
+import { useState } from "react";
 
 export default function ProductPage(){
+  
+  const [IsOpenAdd, setIsOpenAdd] = useState(false)
   return (
     <>
       <Layout title='Product Page'>
         <main>
-          <p>Product Page</p>
-        </main>
+          <section
+          style={{
+            display:"flex",
+            justifyContent:"space-between",
+            alignItems:"center"
+          }}>
+            <Title order={1} style={{marginBottom:"10px"}}>List Product</Title>
+          <Button
+          onClick={()=>setIsOpenAdd(true)}
+          >Add Product
+          </Button>
+          </section>
+          </main>
+          
       </Layout>
     </>
   )
